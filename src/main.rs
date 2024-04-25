1321f0788a57eac50869787ed76b6bf924c8d751
Hildeberto Mendonca <me@hildeberto.com>
2024-04-25 06:17:37 -0400

    Established connection to the database.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use coach::config::load_config;
+use sqlx::postgres::PgPool;
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = load_config().expect("Failed to load config");
     println!("Database URL: {}", config.database.url);
+    let pool = PgPool::connect(&config.database.url).await.expect("Failed to connect to database");
+
+    if !pool.is_closed() {
+        println!("Established connection to the database");
+    }
 }
