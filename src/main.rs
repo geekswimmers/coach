1fcc705d778f49b05e50e6da301b879e2810b3d2
Hildeberto Mendonca <me@hildeberto.com>
2024-04-25 19:30:05 -0400

    Implemented basic upload file

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 extern crate lazy_static;
 
 use actix_files as fs;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_multipart::form::tempfile::TempFile;
+use actix_multipart::form::MultipartForm;
+use actix_web::{web, App, Error, HttpResponse, HttpServer, Responder};
 use coach::config::load_config;
 use sqlx::postgres::PgPool;
+use std::env;
 use tera::{Context, Tera};
 
 lazy_static! {
@@ -19,6 +22,23 @@ lazy_static! {
         let _ = tera.full_reload();
         return tera;
     };
+}
+
+#[derive(Debug, MultipartForm)]
+struct UploadForm {
+    #[multipart(rename = "meet-entries-file")]
+    files: Vec<TempFile>,
+}
+
+async fn save_files(MultipartForm(form): MultipartForm<UploadForm>) -> Result<impl Responder, Error> {
+    let temp_dir = env::temp_dir();
+    for f in form.files {
+        let path = format!("{}/{}", temp_dir.display(), f.file_name.unwrap());
+        println!("Saving to {}", path);
+        f.file.persist(path).unwrap();
+    }
+
+    Ok(HttpResponse::Ok())
 }
 
 async fn home_view() -> impl Responder {
@@ -43,6 +63,7 @@ async fn main() -> std::io::Result<()> {
             App::new()
                 .service(fs::Files::new("/static", "./static").show_files_listing())
                 .route("/", web::get().to(home_view))
+                .route("/meet/results", web::post().to(save_files))
         })
         .bind(("0.0.0.0", config.server_port))?
         .run()
