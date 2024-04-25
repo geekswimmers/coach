5b4dff8d4fe15b0473167ad247eb9dea428011b9
Hildeberto Mendonca <me@hildeberto.com>
2024-04-25 08:43:06 -0400

    Implementing web templates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+#[macro_use]
+extern crate lazy_static;
+
+use actix_files as fs;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use coach::config::load_config;
 use sqlx::postgres::PgPool;
+use tera::{Context, Tera};
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Template parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        let _ = tera.full_reload();
+        return tera;
+    };
+}
+
+async fn home_view() -> impl Responder {
+    let context = Context::new();
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(TEMPLATES.render("index.html", &context).unwrap())
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     let config = load_config().expect("Failed to load config");
     println!("Database URL: {}", config.database.url);
     let pool = PgPool::connect(&config.database.url).await.expect("Failed to connect to database");
@@ -10,4 +38,13 @@ async fn main() {
     sqlx::migrate!("storage/migrations")
         .run(&pool)
         .await.expect("Failed to migrate database");
+
+        HttpServer::new(move || {
+            App::new()
+                .service(fs::Files::new("/static", "./static").show_files_listing())
+                .route("/", web::get().to(home_view))
+        })
+        .bind(("0.0.0.0", config.server_port))?
+        .run()
+        .await
 }
