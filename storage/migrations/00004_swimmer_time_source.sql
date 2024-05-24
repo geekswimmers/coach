a52ad573766b67df75de0bafed77e387f23d8c35
Hildeberto Mendonca <me@hildeberto.com>
2024-05-24 08:40:14 -0400

    Fixed column names in migration file

--- storage/migrations/00004_swimmer_time_source.sql
+++ storage/migrations/00004_swimmer_time_source.sql
@@ -1,4 +1,4 @@
 alter table swimmer_time add if not exists dataset varchar(20);
 drop index if exists idx_swimmer_time;
 
-create unique index if not exists udx_swimmer_time on swimmer_time (swimmer, style, distance, course, time_official, time_date, dataset);
+create unique index if not exists udx_swimmer_time on swimmer_time (swimmer, style, distance, course, official_time, date_time, dataset);
